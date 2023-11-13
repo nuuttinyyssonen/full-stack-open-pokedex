f55ddc664ddf6a3ca08842c38e134711fdde552a
Nuutti <nuutti.nyyssonen@gmail.com>
2023-11-13 23:40:53 +0200

    fixed eslint errors

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
+// eslint-disable-next-line no-use-before-define
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    // eslint-disable-next-line no-use-before-define
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
